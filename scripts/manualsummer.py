9237deb985b2d26514f75f245341a0ee02a9e5cf
evokes <84366755+evokes@users.noreply.github.com>
2022-11-23 12:39:11 -0600

    Update manualsummer.py

--- scripts/manualsummer.py
+++ scripts/manualsummer.py
@@ -2,12 +2,12 @@
 
 import argparse
 from itertools import zip_longest
+import matplotlib.pyplot as plt
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--infile1", help="input file 1 of reference or shifted WYK encoded sequences in string")
 parser.add_argument("--infile2", help="input file 2 of shifted WYK encoded sequences in string")
-parser.add_argument("--isref", help="type true or false if input file 1 is a reference sequence")
-parser.add_argument("--position", help="position of shifted infile 2 where ref seq is added to infile 2")
 parser.add_argument("--outfile", help="outfile name (preferably mentions sum) in string")
 args = parser.parse_args()
 
@@ -17,23 +17,27 @@ with open(args.infile1) as f1:
 file1int = list(map(int, file1))
 
 file1len = len(file1int)
-    
+
+plt.plot(file1int)
+plt.show()
+
 file2 = []
 with open(args.infile2) as f2:
-    file2 = [line.strip() for line in f2] 
+    file2 = [line.strip() for line in f2]
 file2int = list(map(int, file2))
 
+file2len = len(file2int)
+
+plt.plot(file2int)
+plt.show()
+
 finalList = []
-if args.isref == "true":
-    position = int(args.position)
-    slicedfile2 = file2int[position : position + file1len ]
-    sumseq = [x+y for x,y in zip_longest(file1int, slicedfile2, fillvalue=0)]
-    finalList = file2int
-    finalList[position : position + file1len] = sumseq
-elif args.isref == "false":
+if (file1len == file2len):
     finalList = [x+y for x,y in zip_longest(file1int, file2int, fillvalue=0)]
 
 with open(args.outfile, "a") as f:
     for x in finalList:
         f.write(str(x) + "\n")
 
+plt.plot(finalList)
+plt.show()
